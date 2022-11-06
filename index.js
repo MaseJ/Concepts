35e7fb1a49ce573091a367fbc20f8a595450f4ed
MaseJ <mjohnson434@student.cvtc.edu>
2022-11-06 16:52:35 -0600

    Add showcase for all concepts

---
+++ index.js
@@ -0,0 +1,14 @@
+var options = {
+    "animate": true,
+    "patternWidth": 15.4,
+    "patternHeight": 31.61,
+    "grainOpacity": 0.09,
+    "grainDensity": 1,
+    "grainWidth": 1,
+    "grainHeight": 1
+}
+
+grained("#img", options);
+grained("#header", options);
+grained("#content", options);
+
